89a67adb565b8e43995344213e74c23d8de56b36
Saku Karttunen <saku.karttunen@gmail.com>
2024-11-07 19:19:59 +0200

    trying to get started with steering

--- Assets/Scripts/CarScript.cs
+++ Assets/Scripts/CarScript.cs
@@ -1,13 +1,26 @@
 using UnityEngine;
 using UnityEngine.Assertions;
 
+[System.Serializable]
+public record TireAxel
+{
+    public Transform left;
+    public Transform right;
+    public float tireGripFactor;
+    public float tireMass;
+
+    public Transform[] tires => new Transform[]{left, right};
+}
+
 public class CarScript : MonoBehaviour
 {
     [Header("References")]
     [SerializeField]
     private Rigidbody carRb;
     [SerializeField]
-    private Transform[] rayPoints;
+    private TireAxel frontTires;
+    [SerializeField]
+    private TireAxel rearTires;
     [SerializeField]
     private LayerMask driveable;
 
@@ -31,6 +44,7 @@ public class CarScript : MonoBehaviour
 
     // makes sure that the values passed to speed are close to km/h
     private float speedMultiplier;
+    private Transform[] allRayPoints;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -47,37 +61,107 @@ public class CarScript : MonoBehaviour
         // at the end, we count the total force needed to reach the wanted speed (in km/h) from 0 km/h
         // F_total = F_down + (F_acceleration / 3.6)
         speedMultiplier = carForceDownwards + (desiredAcceleration / 3.6f);
+
+        // get all the tire raypoints for suspension usage
+        allRayPoints = new Transform[]{
+            frontTires.left,
+            frontTires.right,
+            rearTires.left,
+            rearTires.right,
+        };
     }
 
     void FixedUpdate()
     {
         Suspension();
-        Control();
+        /*carRb.AddRelativeForce(Vector3.forward *15000);*/
+        Acceleration();
+        Steering();
     }
 
-    private void Control()
+    private void Acceleration()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            carRb.AddRelativeForce(Vector3.forward * (speed * speedMultiplier) * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            carRb.AddRelativeForce(Vector3.left * steeringSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            carRb.AddRelativeForce(Vector3.right * steeringSpeed * Time.deltaTime);
+            foreach (Transform tire in frontTires.tires)
+            {
+                carRb.AddForceAtPosition(tire.forward * (speed * speedMultiplier) * Time.deltaTime, tire.position);
+            }
         }
         if (Input.GetKey(KeyCode.S))
         {
-            carRb.AddRelativeForce(Vector3.back * (speed * speedMultiplier * 2) * Time.deltaTime);
+            foreach (Transform tire in frontTires.tires)
+            {
+                carRb.AddForceAtPosition(-tire.forward * (speed * speedMultiplier * 3) * Time.deltaTime, tire.position);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            foreach (Transform tire in frontTires.tires)
+            {
+                Quaternion rotation = Quaternion.Euler(0, -45, 0);
+                tire.localRotation = rotation;
+            }
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            foreach (Transform tire in frontTires.tires)
+            {
+                Quaternion rotation = Quaternion.Euler(0, 45, 0);
+                tire.localRotation = rotation;
+            }
+        }
+        else
+        {
+            foreach (Transform tire in frontTires.tires)
+            {
+                Quaternion rotation = Quaternion.Euler(0, 0, 0);
+                tire.localRotation = rotation;
+            }
+        }
+    }
+
+    private void Steering() 
+    {
+        foreach (Transform rayPoint in frontTires.tires)
+        {
+            // draw lines to visualize the spring force
+            Debug.DrawLine(rayPoint.position, rayPoint.position + (rayPoint.forward * 10), Color.blue);
+            Debug.DrawLine(rayPoint.position, rayPoint.position + (rayPoint.up * 10), Color.blue);
+
+            RaycastHit hit;
+            float maxLength = restLength + springTravel;
+
+            // check if the car is not on the ground
+            if (!Physics.Raycast(rayPoint.position, -rayPoint.up, out hit, maxLength + wheelRadius, driveable))
+            {
+                Debug.Log("car is not on the ground");
+                return;
+            }
+
+            Vector3 steeringDirection = rayPoint.right;
+            if (Input.GetKey(KeyCode.A))
+                steeringDirection = -rayPoint.right;
+
+            Vector3 tireWorldVelocity = carRb.GetPointVelocity(rayPoint.position);
+            Debug.DrawLine(rayPoint.position, rayPoint.position + (tireWorldVelocity * 10), Color.magenta);
+            Debug.DrawLine(rayPoint.position, rayPoint.position + (steeringDirection * 10), Color.green);
+
+            float steeringVelocity = Vector3.Dot(steeringDirection, tireWorldVelocity);
+
+            float desiredVelocityChange = -steeringVelocity * frontTires.tireGripFactor;
+
+            float desiredAcceleration = desiredVelocityChange / Time.fixedDeltaTime;
+
+            Vector3 netForce = steeringDirection * desiredAcceleration * frontTires.tireMass;
+            carRb.AddForceAtPosition(netForce, rayPoint.position);
         }
     }
 
     private void Suspension()
     {
-        foreach (Transform rayPoint in rayPoints)
+        foreach (Transform rayPoint in allRayPoints)
         {
             RaycastHit hit;
             float maxLength = restLength + springTravel;
