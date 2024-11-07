4dd6cdf33749d6bfc34695c797c6731ce52e79f2
Saku Karttunen <saku.karttunen@gmail.com>
2024-11-07 17:02:50 +0200

    added springs and gridbox for development of the car

--- Assets/Scripts/CarScript.cs
+++ Assets/Scripts/CarScript.cs
@@ -29,6 +29,9 @@ public class CarScript : MonoBehaviour
     [SerializeField]
     private float steeringSpeed = 100;
 
+    // makes sure that the values passed to speed are close to km/h
+    private float speedMultiplier;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -36,6 +39,14 @@ public class CarScript : MonoBehaviour
             carRb = GetComponent<Rigidbody>();
         // check that the rigidbody is not null, throw error if it is
         Assert.IsNotNull(carRb);
+
+        // F_down = mass * gravity
+        float carForceDownwards = carRb.mass * 9.81f;
+        // F_acceleration (m/s) = mass * acceleration
+        float desiredAcceleration = carRb.mass * 1;
+        // at the end, we count the total force needed to reach the wanted speed (in km/h) from 0 km/h
+        // F_total = F_down + (F_acceleration / 3.6)
+        speedMultiplier = carForceDownwards + (desiredAcceleration / 3.6f);
     }
 
     void FixedUpdate()
@@ -48,7 +59,7 @@ public class CarScript : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            carRb.AddRelativeForce(Vector3.forward * speed * Time.deltaTime);
+            carRb.AddRelativeForce(Vector3.forward * (speed * speedMultiplier) * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A))
         {
@@ -60,7 +71,7 @@ public class CarScript : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.S))
         {
-            carRb.AddRelativeForce(Vector3.back * speed * Time.deltaTime);
+            carRb.AddRelativeForce(Vector3.back * (speed * speedMultiplier * 2) * Time.deltaTime);
         }
     }
 
@@ -73,24 +84,31 @@ public class CarScript : MonoBehaviour
 
             if (Physics.Raycast(rayPoint.position, -rayPoint.up, out hit, maxLength + wheelRadius, driveable))
             {
-                Debug.Log("hit!");
                 float currentSpringLength = hit.distance - wheelRadius;
-                float springCompression = restLength - currentSpringLength / springTravel;
+                // how much the spring has compressed from the neutral position
+                float springOffset = restLength - currentSpringLength / springTravel;
 
+                // to calculate the dampening force, we need to know the current velocity of the spring
                 float springVelocity = Vector3.Dot(carRb.GetPointVelocity(rayPoint.position), rayPoint.up);
-                float dampForce = damperStiffness * springVelocity;
 
-                float springForce = springStiffness * springCompression;
+                // F_damp = velocity * dampening
+                float dampForce = springVelocity * damperStiffness;
 
-                float netForce = springForce - dampForce;
+                // F_spring = offset * strength
+                // the offset is the distance form the resting position of the spring
+                // the strength is the firmness of the spring
+                float springForce = springOffset * springStiffness;
 
-                carRb.AddForceAtPosition(netForce * rayPoint.up, rayPoint.position);
+                // F_total = (offset * strength) - (velocity * dampening)
+                float totalForce = springForce - dampForce;
 
+                carRb.AddForceAtPosition(totalForce * rayPoint.up, rayPoint.position);
+
+                // draw lines to visualize the spring force
                 Debug.DrawLine(rayPoint.position, hit.point, Color.red);
             }
             else
             {
-                Debug.Log("not hitting!");
                 Debug.DrawLine(rayPoint.position, rayPoint.position + (wheelRadius + maxLength) * -rayPoint.up, Color.green);
             }
         }
