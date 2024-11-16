fe3104712f62aa5f8b00a9e2fc1dbd4f074ba0d8
Saku Karttunen <saku.karttunen@gmail.com>
2024-11-16 16:36:18 +0200

    added cinemachine cameras

--- Assets/Code/Vehicle/CarController.cs
+++ Assets/Code/Vehicle/CarController.cs
@@ -16,15 +16,6 @@ public class CarController : MonoBehaviour
     [Header("Layer mask")]
     public LayerMask driveableLayer;
 
-    [Header("Camera Settings")]
-    public Camera mainCamera;
-    [SerializeField]
-    private float fovSensitivity = 5;
-    [SerializeField]
-    private float cameraSmoothness = 5f;
-    [SerializeField]
-    private int maxFov = 110;
-
     // states
     private float baseFov;
 
@@ -36,19 +27,5 @@ public class CarController : MonoBehaviour
         // check that the rigidbody is not null, throw error if it is
         Assert.IsNotNull(rigidBody);
         powertrain = GetComponent<Powertrain>();
-        baseFov = mainCamera.fieldOfView;
     }
-
-    void Update()
-    {
-        // make the camera zoom out during high speeds, for better sense of speed
-        float fovMultiplier = powertrain.GetCurrentSpeed() * (fovSensitivity / 10);
-        float targetFov = baseFov + fovMultiplier;
-
-        // clamp the target fov
-        targetFov = Mathf.Clamp(targetFov, 0, maxFov);
-
-        mainCamera.fieldOfView = Mathf.Lerp(mainCamera.fieldOfView, targetFov, Time.deltaTime * cameraSmoothness);
-    }
-
 }
