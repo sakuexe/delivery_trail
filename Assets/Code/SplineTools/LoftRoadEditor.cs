97d396a68a223caa3246e2cbf1bb4ca5f95843d8
Saku Karttunen <saku.karttunen@gmail.com>
2024-11-17 10:47:56 +0200

    added a spline tool for creating roads
    try it out, if it is fun to use and helpful, I can make a better one for
    our use case later.

---
+++ Assets/Code/SplineTools/LoftRoadEditor.cs
@@ -0,0 +1,22 @@
+using UnityEditor;
+
+[CustomEditor(typeof(LoftRoadBehaviour))]
+[CanEditMultipleObjects]
+class SplineWidthEditor : UnityEditor.Editor
+{
+    public override void OnInspectorGUI()
+    {
+        EditorGUI.BeginChangeCheck();
+
+        base.OnInspectorGUI();
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            EditorApplication.delayCall += () =>
+            {
+                foreach (var target in targets)
+                    ((LoftRoadBehaviour)target).LoftAllRoads();
+            };
+        }
+    }
+}
