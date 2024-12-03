ae976961e2a43a530ec2b38e7ee5434cbb320e20
aleksi <aleksi.halttunen@student.hamk.fi>
2024-12-03 19:12:57 +0200

    Menu start to game. settings / exit dont work yet

--- Assets/Scenes/Menu/MainMenuEvents.cs
+++ Assets/Scenes/Menu/MainMenuEvents.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class MainMenuEvents : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    private void OnEnable()
+    {     
+        var root = GetComponent<UIDocument>().rootVisualElement;
+     
+        var startButton = root.Q<Button>("startbutton");
+        var settingsButton = root.Q<Button>("settingsbutton");
+        var exitButton = root.Q<Button>("exitbutton");
+
+        if (startButton != null)
+            startButton.clicked += () => SceneManager.LoadScene("Level 1");
+
+        if (settingsButton != null)
+            settingsButton.clicked += OpenSettings;
+
+        if (exitButton != null)
+            exitButton.clicked += () => Application.Quit();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OpenSettings()
     {
-        
+        Debug.Log("Settings menu opened!");
+        // Lis‰‰ toiminnallisuus asetusvalikon avaamiseen
     }
 }
