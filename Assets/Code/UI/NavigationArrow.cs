1990bb49c13e1e301e60750b8b3e5591d43971df
Saku Karttunen <saku.karttunen@gmail.com>
2024-12-16 15:43:50 +0200

    made the arrow point to the goal when the checkpoints are done

--- Assets/Code/UI/NavigationArrow.cs
+++ Assets/Code/UI/NavigationArrow.cs
@@ -12,11 +12,13 @@ public class NavigationArrow : MonoBehaviour
     {
         SetNextCheckpoint();
         GameManager.Instance.onCheckpointCleared += SetNextCheckpoint;
+        GameManager.Instance.onLevelFinished += HideArrow;
     }
 
     void OnDisable()
     {
         GameManager.Instance.onCheckpointCleared -= SetNextCheckpoint;
+        GameManager.Instance.onLevelFinished -= HideArrow;
     }
 
     void Update()
@@ -29,52 +31,24 @@ public class NavigationArrow : MonoBehaviour
         Vector3 direction = (nextCheckpoint.transform.position - mainCamera.transform.position).normalized;
         Quaternion targetRotation = Quaternion.LookRotation(direction);
 
-        // Find the difference between the target rotation and the camera's current rotation
+        // find the difference between the target rotation and the camera's current rotation
         Quaternion rotationDifference = Quaternion.Inverse(mainCamera.transform.rotation) * targetRotation;
-
-        // Apply this relative rotation to the arrow
+        // apply this relative rotation to the arrow
         arrow.transform.rotation = rotationDifference;
-
-        /*GameManager.Instance.player.transform.rotation = Quaternion.LookRotation(direction);*/
-        /*arrow.transform.rotation = Quaternion.FromToRotation(mainCamera.transform.position, nextCheckpoint.transform.position);*/
-
-        Debug.DrawLine(GameManager.Instance.player.transform.position, nextCheckpoint.transform.position, Color.red);
     }
 
     private void SetNextCheckpoint()
     {
-        nextCheckpoint = GetClosestCheckpoint();
+        int clearedCheckpoints = GameManager.Instance.checkpointsCleared.Count - 1;
+        if (clearedCheckpoints >= GameManager.Instance.checkpoints.Length)
+            nextCheckpoint = GameObject.FindWithTag("Goal");
+        else 
+            nextCheckpoint = GameManager.Instance.checkpoints[clearedCheckpoints];
         Debug.Log($"Next checkpoint is: {nextCheckpoint.name}");
     }
 
-    private GameObject GetClosestCheckpoint()
+    private void HideArrow()
     {
-        GameObject closestCheckpoint = null;
-        Vector3 playerPosition = GameManager.Instance.player.transform.position;
-        foreach (GameObject checkpoint in GameManager.Instance.checkpoints)
-        {
-            if (IsCheckpointCleared(checkpoint))
-                continue;
-            if (!closestCheckpoint)
-            {
-                closestCheckpoint = checkpoint;
-                continue;
-            }
-
-            float distanceToCheckpoint = Vector3.Distance(playerPosition, checkpoint.transform.position);
-            if (distanceToCheckpoint < Vector3.Distance(playerPosition, closestCheckpoint.transform.position))
-                closestCheckpoint = checkpoint;
-        }
-        return closestCheckpoint;
-    }
-
-    private bool IsCheckpointCleared(GameObject checkpoint)
-    {
-        foreach (Checkpoint clearedCheckpoint in GameManager.Instance.checkpointsCleared)
-        {
-            if (clearedCheckpoint.position == checkpoint.transform.position)
-                return true;
-        }
-        return false;
+        arrow.SetActive(false);
     }
 }
